71d34467bf936c5cabf852d15576376cf3efefe5
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2024-02-11 17:24:39 +0530

    Updates

--- ModuleDetailPage.3156ba79.js
+++ ModuleDetailPage.3156ba79.js
@@ -836,7 +836,7 @@ const NOTHING_HERE = /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
 }, undefined);
 const TwoColListAndRankView = ({ Component1, Component2 })=>{
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
-        className: "flex flex-col md:flex-row justify-center md:p-20 sm:p-2 bg-[#fcfcfd] h-full w-full",
+        className: "flex flex-col sm:flex-row sm:gap-6 justify-center md:p-20 sm:p-2 bg-[#fcfcfd] h-full w-full",
         children: [
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
                 className: "grow shadow-md rounded-md bg-white",
